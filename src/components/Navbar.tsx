e1f43f906036f4bf2afecac4cf5be5af6e16a443
zita-b <120709795+zita-b@users.noreply.github.com>
2023-01-06 21:43:33 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,9 +14,6 @@ export function Navbar() {
     <NavbarBootstrap sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
-          <Nav.Link to="/" as={NavLink}>
-            Home
-          </Nav.Link>
           <Nav.Link to="/store" as={NavLink}>
             Store
           </Nav.Link>
