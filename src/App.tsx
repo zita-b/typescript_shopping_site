d8002b02f29844315173a4c9ed503d44a1a3ecf5
zita-b <120709795+zita-b@users.noreply.github.com>
2023-01-06 21:41:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
@@ -12,10 +11,7 @@ function App() {
       <Navbar />
       <Container className="mb-4">
         <Routes>
-          <Route path="/" element={<Home />}>
-            Home
-          </Route>
-          <Route path="/store" element={<Store />}>
+          <Route path="/" element={<Store />}>
             Store
           </Route>
           <Route path="/about" element={<About />}>
